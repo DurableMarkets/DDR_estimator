DurableMarkets/DDR_estimator

commit 4f4d8e05d146ffe2cb2bbff828ba425464b5cf74

fixed a bug where scrap correction could not be created when dimension was (1,1) ie. a scalar. ie common across consumer types

src/logit/prices/prices.py
@@ -127,10 +127,11 @@ def create_scrap_correction(
 
     # scrap correction terms 
     for ntype in range(0, options["n_consumer_types"]):
+        for ncartype in range(1, options['n_car_types']+1):
             X.loc[
-                pd.IndexSlice[ntype, :, :, :, :], scrap_cols_flat[ntype]
+                pd.IndexSlice[ntype, :, :, ncartype, :], scrap_cols_looper[ntype][ncartype-1]
             ] = (
-                scrap_correct.loc[pd.IndexSlice[ntype, :, :, :, :], 'scrap_correction'] 
+                scrap_correct.loc[pd.IndexSlice[ntype, :, :, ncartype, :], 'scrap_correction'].values                 
             )
     X = X.loc[:, scrap_cols_flat]
 

commit 8fbd3e9e3b76818e7afedb8fb77b6dff9efaec35

fixed critical bug introduced with  4f4d8e05d146ffe2cb2bbff828ba425464b5cf74

src/logit/ddr_tools/dependent_vars.py
@@ -21,6 +21,11 @@ def true_ccps(main_df, model_solution, options):
         ccps.index.get_level_values('state').values,
         ccps.index.get_level_values('decision').values,
         ]
+    
+    # reformat: This is dumb - should just make it use the main index 
+    ccps = ccps.loc[:,'ccps']
+    ccps=ccps.reset_index().set_index(['consumer_type', 'state', 'decision'])
+    ccps=ccps.loc[:, "ccps"]
 
     # ccps['ccps_scrap'] = model_solution["ccps_scrap_tau"][
     #     ccps.index.get_level_values('consumer_type').values, 
@@ -28,7 +33,6 @@ def true_ccps(main_df, model_solution, options):
     #     ]
 
 
-    ccps=ccps.loc[:, "ccps"]
 
     return ccps
 

src/logit/monte_carlo_tools/misc_tools.py
@@ -8,7 +8,7 @@ def construct_price_dict(equ_price, state_space_arrays, params, options):
     prices = {
         "used_car_prices": np.array(equ_price),
         "new_car_prices": np.array(params["pnew"]),
-        "scrap_car_price": np.array(params["pscrap"]),
+        "scrap_car_prices": np.array(params["pscrap"]),
         "used_prices_indexer": np_state_decision_arrays["map_state_to_price_index"],
     }
 

src/logit/prices/prices.py
@@ -127,12 +127,15 @@ def create_scrap_correction(
 
     # scrap correction terms 
     for ntype in range(0, options["n_consumer_types"]):
-        for ncartype in range(1, options['n_car_types']+1):
+        for ncartype in range(0, options['n_car_types']+1): # IMPORTANT ZERO IS INCLUDED
+
+            # This does not work
             X.loc[
                 pd.IndexSlice[ntype, :, :, ncartype, :], scrap_cols_looper[ntype][ncartype-1]
             ] = (
                 scrap_correct.loc[pd.IndexSlice[ntype, :, :, ncartype, :], 'scrap_correction'].values                 
             )
+    
     X = X.loc[:, scrap_cols_flat]
-
+       
     return X, scrap_cols_flat